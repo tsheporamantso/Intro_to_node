f912d5b9e6ac6d76a3ac89936c37b0499f38c152
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-19 16:29:49 +0200

    Working with imports and exports of modules and file sharing

---
+++ modules.js
@@ -0,0 +1,10 @@
+const greet = require("./modules/greet");
+const { tshepo, dumi } = require("./modules/names"); // I prefer to destructor names as opposed to calling the property when invoking the function. e.g names.tshepo
+const alternativeFlavor = require("./modules/alternative-flavor");
+
+greet("Lebo");
+greet(tshepo);
+greet(dumi);
+
+console.log(alternativeFlavor.items);
+console.log(alternativeFlavor.singlePerson);
---
+++ modules/names.js
@@ -0,0 +1,7 @@
+const tshepo = "Tshepo";
+const dumi = "Dumisani";
+
+module.exports = {
+  tshepo,
+  dumi,
+};
