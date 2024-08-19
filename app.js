b2e63ef4dd06a0993a38a15506258c5acca05294
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-19 17:53:42 +0200

    Breaf lesson on built in os module

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const os = require("os");
 
+// Method returns user info
+const user = os.userInfo();
+console.log(user);
+
+// Method returns system uptim in seconds
+const runtime = os.uptime();
+
+console.log(`The sytems uptime ${runtime} in seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
