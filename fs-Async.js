9e21c83f737aa624acbfb38b5423bea2f7985934
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-20 16:51:26 +0200

    Add event loop notes and examples

--- fs-Async.js
+++ fs-Async.js
@@ -1,21 +1,30 @@
 const { readFile, writeFile } = require('fs');
 
+/*
+! Below example of event loop in asynchronous read and write files!
+*/
+
+// console.log('first task');
 readFile(`${__dirname}/content/first.txt`, 'utf8', (err, data) => {
   if (err) {
     throw err;
   } else {
     const first = data;
+    // console.log(first);
     readFile(`${__dirname}/content/second.txt`, 'utf8', (err, data) => {
       if (err) {
         throw err;
       } else {
         const second = data;
+        // console.log(second);
         writeFile(
           `${__dirname}/content/result-async.txt`,
           `${first}\n${second}`,
           (err) => {
             if (err) {
               throw err;
+            } else {
+              // console.log('next task');
             }
           },
         );
@@ -23,3 +32,4 @@ readFile(`${__dirname}/content/first.txt`, 'utf8', (err, data) => {
     });
   }
 });
+// console.log('second task');
