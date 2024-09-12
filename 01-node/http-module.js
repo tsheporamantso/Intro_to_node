09c3351ff1d90a9b9b2ee2c20591fa57b67b1793
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-12 19:14:30 +0200

    Refactor routes http status codes

--- 01-node/http-module.js
+++ 01-node/http-module.js
@@ -1,21 +1,22 @@
 const http = require('http');
 const url = require('url');
+const { StatusCodes } = require('http-status-codes');
 
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url);
 
   if (pathname === '/' || pathname === '/home') {
-    res.writeHead(200, {
+    res.writeHead(StatusCodes.OK, {
       'Content-type': 'text/html',
     });
     res.end('<h1>Welcome to home page</h1>');
   } else if (pathname === '/about') {
-    res.writeHead(200, {
+    res.writeHead(StatusCodes.OK, {
       'Content-type': 'text/html',
     });
     res.end('<h1>Welcome to the about page!</h1>');
   } else {
-    res.writeHead(404, {
+    res.writeHead(StatusCodes.NOT_FOUND, {
       'Content-type': 'text/html',
     });
     res.end(`
